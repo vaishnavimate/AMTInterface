6fc0abbcbfd8e1d4676b1507bf6dc0e23eacf716
Vaishnavi Mate <159476791+vaishnavimate@users.noreply.github.com>
2024-03-04 23:17:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.atm;
+import java.sql.SQLException;
+
+public class Main {
+	
+	
+
+	
+		public static void main(String[] args) throws InterruptedException, SQLException {
+			Login login = new Login();
+			login.loginView();
+		}
+	}
+
+
